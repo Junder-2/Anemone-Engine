2ebf13707d0c092fc5d45e4c97eeda8b6670d76b
Nestorboy <35258953+Nestorboy@users.noreply.github.com>
2024-03-22 00:07:29 +0100

    Made LibraryDir and IncludeDir paths absolute

--- premakeDependencies.lua
+++ premakeDependencies.lua
@@ -1,9 +1,12 @@
 -- Engine Dependencies
 
+External = {}
+External["SDL2"] = "%{wks.location}/%{prj.name}/vendor/SDL2-2.30.1"
+
 -- Include Directories
 IncludeDir = {}
-IncludeDir["SDL2"] = "%{prj.name}/vendor/SDL2-2.30.1/include"
+IncludeDir["SDL2"] = "%{External.SDL2}/include"
 
 -- Library Directories
 LibraryDir = {}
-LibraryDir["SDL2"] = "%{prj.name}/vendor/SDL2-2.30.1/lib/x64"
+LibraryDir["SDL2"] = "%{External.SDL2}/lib/x64"
