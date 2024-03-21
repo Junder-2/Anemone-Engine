adc8a801c74c0ca66bbfa0e6805ba8fe915ac053
Nestorboy <35258953+Nestorboy@users.noreply.github.com>
2024-03-21 22:00:40 +0100

    Began replacing main with a proper entry

---
+++ Engine/src/NP/EntryPoint.cpp
@@ -0,0 +1,15 @@
+#include <SDL.h>
+#include <cstdio>
+
+//int main(int arg, char* argv[])
+//{
+//	SDL_Init(SDL_INIT_TIMER);
+//
+//	printf("Hello\n");
+//
+//	SDL_Delay(100);
+//
+//	SDL_Quit();
+//
+//	return 1;
+//}
