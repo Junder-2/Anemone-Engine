040801035d5e8ec704371451f220472f06410ed4
Nestorboy <35258953+Nestorboy@users.noreply.github.com>
2024-03-21 17:41:26 +0100

    Added dedicated Premake dependency file for paths

---
+++ premakeDependencies.lua
@@ -0,0 +1,9 @@
+-- Engine Dependencies
+
+-- Include Directories
+IncludeDir = {}
+IncludeDir["SDL2"] = "%{prj.name}/vendor/SDL2-2.30.1/include"
+
+-- Library Directories
+LibraryDir = {}
+LibraryDir["SDL2"] = "%{prj.name}/vendor/SDL2-2.30.1/lib/x64"
