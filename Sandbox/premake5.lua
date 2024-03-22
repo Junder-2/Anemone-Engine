ea04c8fa1131293dc2315991fcf7038a6d553b25
Nestorboy <35258953+Nestorboy@users.noreply.github.com>
2024-03-22 16:21:22 +0100

    Added Premake script for the Sandbox
    • Copied "Sandbox" Premake project over to Sandbox/premake5.lua.
    • Moved staticruntime out of filter.
    • Moved spdlog up to the top in include directories.
    • Made target and obj directories use wks.location.

---
+++ Sandbox/premake5.lua
@@ -0,0 +1,38 @@
+project "Sandbox"
+    location "Sandbox"
+    kind "ConsoleApp"
+    language "C++"
+    cppdialect "C++20"
+    staticruntime "on"
+
+    targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("%{wks.location}/bin-intermediate/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "src/**.h",
+        "src/**.cpp"
+    }
+
+    includedirs
+    {
+        "%{wks.location}/Engine/vendor/spdlog/include",
+        "%{IncludeDir.SDL2}",
+        "%{IncludeDir.ImGui}",
+        "Engine/src"
+    }
+
+    links
+    {
+        "Engine"
+    }
+
+    filter "system:windows"
+        systemversion "latest"
+
+    filter "configurations:Debug"
+        defines "NP_DEBUG"
+        symbols "on"
+
+    filter "configurations:Release"
+        optimize "on"
