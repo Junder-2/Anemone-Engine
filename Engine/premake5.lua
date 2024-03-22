dfa85e46bf2e9fc8d12e665b91f9b2d41d0a936f
Nestorboy <35258953+Nestorboy@users.noreply.github.com>
2024-03-22 16:13:36 +0100

    Added separate Engine Premake script
    • Copied "Engine" Premake project over to Engine/premake5.lua.
    • Moved staticruntime out of filter.
    • Moved spdlog up to the top in include directories.
    • Made target and obj directories use wks.location.

---
+++ Engine/premake5.lua
@@ -0,0 +1,54 @@
+project "Engine"
+    location "Engine"
+    kind "SharedLib" --Engine will be DLL
+    language "C++"
+    cppdialect "C++20"
+    staticruntime "on"
+
+    targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("%{wks.location}/bin-intermediate/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "src/**.h",
+        "src/**.cpp"
+    }
+
+    includedirs
+    {
+        "vendor/spdlog/include",
+        "%{IncludeDir.SDL2}",
+        "%{IncludeDir.ImGui}",
+    }
+
+    libdirs
+    {
+        "%{LibraryDir.SDL2}"
+    }
+
+    links
+    {
+        "SDL2",
+        "ImGui"
+    }
+
+    filter "system:windows"
+        systemversion "latest"
+
+        defines
+        {
+            "NP_BUILD_DLL"
+        }
+
+        postbuildcommands
+        {
+            ("{COPY} %{cfg.buildtarget.relpath} \"../bin/" .. outputdir .. "/Sandbox/\""),
+            ("{COPY} \"%{LibraryDir.SDL2}/SDL2.dll\" \"../bin/" .. outputdir .. "/Sandbox/\"")
+        }
+
+    filter "configurations:Debug"
+        defines "NP_DEBUG"
+        symbols "on"
+
+    filter "configurations:Release"
+        optimize "on"
