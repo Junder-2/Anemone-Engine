e0cd1786c2f01b0a58f904e1c1f601a863522fdd
Nestorboy <35258953+Nestorboy@users.noreply.github.com>
2024-03-22 16:44:08 +0100

    Removed "Sandbox" project from root Premake script
    • Root Premake script now includes Sandbox Premake script.

--- premake5.lua
+++ premake5.lua
@@ -19,41 +19,6 @@ group "Core"
     include "Engine"
 group ""
 
-project "Sandbox"
-    location "Sandbox"
-    kind "ConsoleApp"
-    language "C++"
-    cppdialect "C++20"
-
-    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-    objdir ("bin-intermediate/" .. outputdir .. "/%{prj.name}")
-
-    files
-    {
-        "%{prj.name}/src/**.h",
-        "%{prj.name}/src/**.cpp"
-    }
-
-    includedirs
-    {
-        "%{IncludeDir.SDL2}",
-        "%{IncludeDir.ImGui}",
-        "%{wks.location}/Engine/vendor/spdlog/include",
-        "Engine/src"
-    }
-
-    links
-    {
-        "Engine"
-    }
-
-    filter "system:windows"
-        staticruntime "on"
-        systemversion "latest"
-
-    filter "configurations:Debug"
-        defines "NP_DEBUG"
-        symbols "on"
-
-    filter "configurations:Release"
-        optimize "on"
+group "Misc"
+    include "Sandbox"
+group ""
