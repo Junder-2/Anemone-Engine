64522ac8c8c20eae6f3762004ec422e04768aaa0
Nestorboy <35258953+Nestorboy@users.noreply.github.com>
2024-03-22 16:15:19 +0100

    Removed "Engine" project from root Premake script
    • Root Premake script now includes Engine Premake script.

--- premake5.lua
+++ premake5.lua
@@ -15,60 +15,9 @@ group "Dependencies"
     include "Engine/vendor/imgui-1.90.4"
 group ""
 
-project "Engine"
-    location "Engine"
-    kind "SharedLib" --Engine will be DLL
-    language "C++"
-    cppdialect "C++20"
-
-    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-    objdir ("bin-intermediate/" .. outputdir .. "/%{prj.name}")
-
-    files
-    {
-        "%{prj.name}/src/**.h",
-        "%{prj.name}/src/**.cpp"
-    }
-
-    includedirs
-    {
-        "%{IncludeDir.SDL2}",
-        "%{IncludeDir.ImGui}",
-        "%{prj.name}/vendor/spdlog/include"
-    }
-
-    libdirs
-    {
-        "%{LibraryDir.SDL2}"
-    }
-
-    links
-    {
-        "SDL2",
-        "ImGui"
-    }
-
-    filter "system:windows"
-        staticruntime "on"
-        systemversion "latest"
-
-        defines
-        {
-            "NP_BUILD_DLL"
-        }
-
-        postbuildcommands
-        {
-            ("{COPY} %{cfg.buildtarget.relpath} \"../bin/" .. outputdir .. "/Sandbox/\""),
-            ("{COPY} \"%{LibraryDir.SDL2}/SDL2.dll\" \"../bin/" .. outputdir .. "/Sandbox/\"")
-        }
-
-    filter "configurations:Debug"
-        defines "NP_DEBUG"
-        symbols "on"
-
-    filter "configurations:Release"
-        optimize "on"
+group "Core"
+    include "Engine"
+group ""
 
 project "Sandbox"
     location "Sandbox"
