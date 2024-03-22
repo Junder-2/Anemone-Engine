e9489c11f166833cc35477586be16551b9a14a6e
Nestorboy <35258953+Nestorboy@users.noreply.github.com>
2024-03-22 18:41:20 +0100

    Added simple Vulkan check to GenerateProjects.bat

--- GenerateProjects.bat
+++ GenerateProjects.bat
@@ -1,2 +1,9 @@
-call vendor\premake\premake5.exe vs2022
-PAUSE
+@echo off
+
+if "%VULKAN_SDK%" == "" (
+    echo No valid Vulkan SDK installation found. Please install Vulkan before proceeding.
+    PAUSE
+) else (
+    call vendor\premake\premake5.exe vs2022
+    PAUSE
+)
