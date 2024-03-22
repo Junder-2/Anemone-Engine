bbc2e6fa49173a2a65e0799f468e06ac0c5e471f
Nestorboy <35258953+Nestorboy@users.noreply.github.com>
2024-03-22 22:03:34 +0100

    Added spdlog directory to dependencies table

--- premakeDependencies.lua
+++ premakeDependencies.lua
@@ -6,11 +6,13 @@ External = {}
 External["SDL2"] = "%{wks.location}/Engine/vendor/SDL2-2.30.1"
 External["ImGui"] = "%{wks.location}/Engine/vendor/imgui-1.90.4"
 External["GLM"] = "%{wks.location}/Engine/vendor/GLM"
+External["spdlog"] = "%{wks.location}/Engine/vendor/spdlog"
 
 -- Include Directories
 IncludeDir = {}
 IncludeDir["SDL2"] = "%{External.SDL2}/include"
 IncludeDir["ImGui"] = "%{External.ImGui}"
+IncludeDir["spdlog"] = "%{External.spdlog}/include"
 IncludeDir["VulkanSDK"] = "%{VULKAN_SDK}/Include"
 
 -- Library Directories
