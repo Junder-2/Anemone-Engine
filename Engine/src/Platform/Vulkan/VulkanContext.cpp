c28dd3d063b25c08a03bd766e8805fbb2bd866df
Nestorboy <35258953+Nestorboy@users.noreply.github.com>
2024-03-28 18:56:35 +0100

    Added a RenderingContext for Vulkan rendering

---
+++ Engine/src/Platform/Vulkan/VulkanContext.cpp
@@ -0,0 +1,14 @@
+#include "nppch.h"
+#include "VulkanContext.h"
+
+#include <SDL.h>
+#include <SDL_vulkan.h>
+#include <vulkan/vulkan.h>
+#include <imgui.h>
+#include <imgui_impl_sdl2.h>
+#include <imgui_impl_vulkan.h>
+
+namespace Engine
+{
+    
+}
---
+++ Engine/src/Platform/Vulkan/VulkanContext.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "../../NP/Renderer/RenderingContext.h"
+
+typedef struct SDL_Window SDL_Window;
+
+namespace Engine
+{
+    class VulkanContext : public RenderingContext
+    {
+    public:
+        VulkanContext(SDL_Window* windowHandle);
+
+        virtual void Init() override;
+        virtual void SwapBuffers() override;
+        virtual void Cleanup() override;
+
+    private:
+        SDL_Window* _windowHandle;
+    };
+}
