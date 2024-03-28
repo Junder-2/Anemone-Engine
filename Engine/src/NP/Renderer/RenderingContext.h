4442e175d0eb60af7f023f6e13539ff3f010186d
Nestorboy <35258953+Nestorboy@users.noreply.github.com>
2024-03-28 19:00:58 +0100

    Changed to pure virtual functions in RenderingContext

--- Engine/src/NP/Renderer/RenderingContext.h
+++ Engine/src/NP/Renderer/RenderingContext.h
@@ -7,8 +7,8 @@ namespace Engine
     public:
         virtual ~RenderingContext() = default;
 
-        virtual void Init();
-        virtual void SwapBuffers();
-        virtual void Cleanup();
+        virtual void Init() = 0;
+        virtual void SwapBuffers() = 0;
+        virtual void Cleanup() = 0;
     };
 }
