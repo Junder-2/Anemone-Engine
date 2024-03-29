d71eb280a0d5f2f00a5931149513c6e5352ac740
Junder-2 <pettermikaelsson@gmail.com>
2024-03-29 14:44:28 +0100

    Moved bit macros into commonutilities

--- Engine/src/NP/Input/InputTypes.h
+++ Engine/src/NP/Input/InputTypes.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "../Utilities/CommonUtilities.h"
 
 namespace Engine
 {
---
+++ Engine/src/NP/Utilities/CommonUtilities.h
@@ -0,0 +1,4 @@
+#pragma once
+
+#define BIT(x) (1 << (x))
+#define TWO_BIT_MASK(x) ((0b11 << ((x)*2)))
