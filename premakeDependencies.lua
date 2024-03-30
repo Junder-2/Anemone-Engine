c72ab5ebe2b48f6cb34fbb63e7a843c0d2728dc8
Junder-2 <pettermikaelsson@gmail.com>
2024-03-30 22:41:39 +0100

    Moved entt library to engine/vendor

--- premakeDependencies.lua
+++ premakeDependencies.lua
@@ -7,6 +7,7 @@ External["SDL2"] = "%{wks.location}/Engine/vendor/SDL2"
 External["ImGui"] = "%{wks.location}/Engine/vendor/imgui"
 External["GLM"] = "%{wks.location}/Engine/vendor/GLM"
 External["spdlog"] = "%{wks.location}/Engine/vendor/spdlog"
+External["entt"] = "%{wks.location}/Engine/vendor/entt"
 
 
 -- Include Directories
@@ -16,7 +17,7 @@ IncludeDir["ImGui"] = "%{External.ImGui}"
 IncludeDir["GLM"] = "%{External.GLM}"
 IncludeDir["spdlog"] = "%{External.spdlog}/include"
 IncludeDir["VulkanSDK"] = "%{VULKAN_SDK}/Include"
-IncludeDir["entt"] = "%{wks.location}/vendor/entt/include" -- should be correct, include directories in engine shows correct.
+IncludeDir["entt"] = "%{External.entt}/include"
 
 -- Library Directories
 LibraryDir = {}
