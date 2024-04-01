77e3e8c2897932c8b709684db12e4193f0739374
Nestorboy <35258953+Nestorboy@users.noreply.github.com>
2024-04-01 16:43:30 +0200

    Added a Vulkan utility class

---
+++ Engine/src/Platform/Vulkan/VulkanUtils.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <vulkan/vulkan.h>
+
+namespace VulkanUtils
+{
+    void TransitionImage(VkCommandBuffer cmd, VkImage image, VkImageLayout currentLayout, VkImageLayout newLayout);
+
+    void CopyImageToImage(VkCommandBuffer cmd, VkImage source, VkImage destination, VkExtent2D srcSize, VkExtent2D dstSize);
+}
