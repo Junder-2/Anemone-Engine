ef4c47c1b70ed3eb8f7e510927603e2a2499d4e6
Nestorboy <35258953+Nestorboy@users.noreply.github.com>
2024-04-05 14:25:59 +0200

    Added ImGui docking finalization functions

--- Engine/src/NP/Renderer/Renderer.cpp
+++ Engine/src/NP/Renderer/Renderer.cpp
@@ -36,5 +36,11 @@ namespace Engine
     void Renderer::EndUIDataBuffer()
     {
         ImGui::Render();
+
+        if (_vulkanRenderer->GetImGuiIO()->ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+        {
+            ImGui::UpdatePlatformWindows();
+            ImGui::RenderPlatformWindowsDefault();
+        }
     }
 }
