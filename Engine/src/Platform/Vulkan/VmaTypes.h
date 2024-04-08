664f638a87d92935d21471054d23c3c75a9d4e21
Nestorboy <35258953+Nestorboy@users.noreply.github.com>
2024-04-08 18:01:34 +0200

    Added some basic VMA container structs

---
+++ Engine/src/Platform/Vulkan/VmaTypes.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+
+#include <vulkan/vulkan.h>
+#include <vk_mem_alloc.h>
+
+namespace Engine
+{
+    struct VmaImage
+    {
+        VkImage image;
+        VkImageView imageView;
+        VmaAllocation allocation;
+        VkExtent3D imageExtent;
+        VkFormat imageFormat;
+    };
+}
