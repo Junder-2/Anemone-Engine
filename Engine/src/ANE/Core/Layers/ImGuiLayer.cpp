4754906d4af5a4f826928298a86f3ee8a084e515
Gustav Carlberg <55480495+Litene@users.noreply.github.com>
2024-04-12 09:01:25 +0200

    ImGuiLayer
    ImGuiLayer

---
+++ Engine/src/ANE/Core/Layers/ImGuiLayer.cpp
@@ -0,0 +1,46 @@
+﻿#include "anepch.h"
+#include "ImGuiLayer.h"
+
+#include "imgui.h"
+
+Engine::ImGuiLayer::ImGuiLayer(const char* layerName): Layer(layerName)//, _flags(ImGuiUtilities::ImGuiDockNodeFlags_None)
+{
+    _flags = static_cast<ImGuiUtilities::ImGuiDockNodeFlags>(_flags | ImGuiDockNodeFlags_KeepAliveOnly);
+    // ImGuiID ID = ImGui::GetID("DemoDsockSpace");
+    // ImGui::id
+   // ImGui::DockSpace(ID, ImVec2(100, 100), _flags);
+}
+
+Engine::ImGuiLayer::~ImGuiLayer()
+{
+}
+
+void Engine::ImGuiLayer::OnAttach()
+{
+}
+
+void Engine::ImGuiLayer::OnDetach()
+{
+
+
+}
+
+void Engine::ImGuiLayer::OnUpdate(float deltaTime)
+{
+    //1
+
+}
+
+void Engine::ImGuiLayer::OnEvent(Event& e)
+{
+
+}
+
+void Engine::ImGuiLayer::OnUIRender()
+{
+    //2
+    ImGui::Begin("We have a window");
+
+    ImGui::End();
+
+}
---
+++ Engine/src/ANE/Core/Layers/ImGuiLayer.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+#include "Layer.h"
+#include "ANE/Utilities/ImGuiUtilities.h"
+
+namespace Engine
+{
+    class ImGuiLayer : public Layer
+    {
+    public:
+        ImGuiLayer(const char* layerName);
+        ~ImGuiLayer() override;
+        void OnAttach() override;
+        void OnDetach() override;
+        void OnUpdate(float deltaTime) override;
+        void OnEvent(Event& e) override;
+        void OnUIRender() override;
+
+    private:
+        ImGuiUtilities::ImGuiDockNodeFlags _flags;
+    };
+}
