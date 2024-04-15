90e6592bc3196ddc7f8b5a00ebb2823738d302f7
Junder-2 <pettermikaelsson@gmail.com>
2024-04-15 17:57:23 +0200

    Fixed inverted condition

--- Engine/src/ANE/Core/Assert.h
+++ Engine/src/ANE/Core/Assert.h
@@ -7,7 +7,7 @@
 
     #define ANE_DEBUG_BREAK() __debugbreak()
 
-    #define ANE_INTERNAL_ASSERT_IMPL(logger, cond, msg) { if(!(cond)) { logger("Assert failed: {0}", msg); ANE_DEBUG_BREAK(); } }
+    #define ANE_INTERNAL_ASSERT_IMPL(logger, cond, msg) { if((cond)) { logger("Assert failed: {0}", msg); ANE_DEBUG_BREAK(); } }
 
     #define ANE_EASSERT(cond, msg) ANE_INTERNAL_ASSERT_IMPL(ANE_ELOG_ERROR, cond, msg)
     #define ANE_ASSERT(cond, msg) ANE_INTERNAL_ASSERT_IMPL(ANE_LOG_ERROR, cond, msg)
