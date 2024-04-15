e198d861752165c591b1a04634b7106b1016eb1e
Junder-2 <pettermikaelsson@gmail.com>
2024-04-15 13:09:11 +0200

    Added more comments to input classes

--- Engine/src/ANE/Input/Input.h
+++ Engine/src/ANE/Input/Input.h
@@ -5,10 +5,22 @@ namespace Engine
     class InputSystem;
     class EditorInputSystem;
 
+    /**
+    * Returns the editor Inputsystem (This takes priority over app inputsystem)
+    */
     EditorInputSystem& GetEditorInputSystem();
+    /**
+    * Returns the app Inputsystem
+    */
     InputSystem& GetInputSystem();
 
+    /**
+    * Hides mouse/constrains it to the window
+    */
     void HideMouse();
+    /**
+    * Shows mouse/allows mouse to exit window
+    */
     void ShowMouse();
 }
 
--- Engine/src/ANE/Input/InputSystem.h
+++ Engine/src/ANE/Input/InputSystem.h
@@ -11,37 +11,95 @@ namespace Engine
         InputSystem();
         ~InputSystem() override = default;
 
+        /**
+        * Binds an input to a method (Unregistered inputs will be auto registered)
+        * @param bindingPair holds device type and binding id
+        * @param delegateMember method with void(InputValue)
+        */
         template <class TClass>
         void BindInput(BindingPair bindingPair, DelegateMember<TClass, void(InputValue)> delegateMember);
 
+        /**
+        * Binds two inputs to a method (Unregistered inputs will be auto registered)
+        * @param negativeBindingPair holds device type and binding id, decides negative sign
+        * @param positiveBindingPair holds device type and binding id, decides positive sign
+        * @param delegateMember method with void(InputValue)
+        */
         template <class TClass>
         void BindAxisInput(BindingPair negativeBindingPair, BindingPair positiveBindingPair, DelegateMember<TClass, void(InputValue)> delegateMember);
 
+        /**
+        * Binds a keyboard input to a method (Unregistered inputs will be auto registered)
+        * @param keyCode the keycode of the input
+        * @param delegateMember method with void(InputValue)
+        */
         template <class TClass>
         void BindKeyboardInput(KeyCodes keyCode, DelegateMember<TClass, void(InputValue)> delegateMember);
+
+        /**
+        * Binds a keyboard input to a method (Unregistered inputs will be auto registered)
+        * @param negativeKeyCode the keycode of the input, decides negative sign
+        * @param positiveKeyCode the keycode of the input, decides positive sign
+        * @param delegateMember method with void(InputValue)
+        */
         template <class TClass>
         void BindKeyboardAxisInput(KeyCodes negativeKeyCode, KeyCodes positiveKeyCode, DelegateMember<TClass, void(InputValue)> delegateMember);
 
+        /**
+        * Binds mouse movement input to a method
+        * @param delegateMember method with void(MouseMoveValue)
+        */
         template <class TClass>
         void BindMouseMove(DelegateMember<TClass, void(MouseMoveValue)> delegateMember);
 
+        /**
+        * Binds a mouse button input to a method
+        * @param buttonIndex MouseButton
+        * @param delegateMember method with void(InputValue)
+        */
         template <class TClass>
         void BindMouseButton(MouseButton buttonIndex, DelegateMember<TClass, void(InputValue)> delegateMember);
 
+        /**
+        * Binds any mouse button input to a method
+        * @param delegateMember method with void(MouseButtonValues)
+        */
         template <class TClass>
         void BindAnyMouseButton(DelegateMember<TClass, void(MouseButtonValues)> delegateMember);
 
+        /**
+        * Binds a mouse scroll to a method
+        * @param delegateMember method with void(float, float)
+        */
         template <class TClass>
         void BindMouseScroll(DelegateMember<TClass, void(float, float)> delegateMember);
 
+        /**
+        * Returns the trigger state of a key. If the requested key isn't registered returns only pressed or not pressed
+        */
         TriggerState GetKeyTriggerState(const int keyCode) const { return _inputHandler->GetKeyTriggerState(keyCode); }
         // todo: just using for debug now. probably dirty to use array
         std::array<InputValue, 4> GetCurrentTriggeredKeys() const { return _inputHandler->GetCurrentTriggeredKeys(); }
 
+        /**
+        * Returns current mouse delta
+        */
         Vector2 GetMouseDelta() const { return _inputHandler->GetMouseInputData().GetMoveValue().GetMouseDelta(); }
+        /**
+        * Returns current mouse pos
+        */
         Vector2 GetMousePos() const { return _inputHandler->GetMouseInputData().GetMoveValue().GetMousePos(); }
+        /**
+        * Returns current mouse scroll deltas
+        */
         Vector2 GetMouseScroll() const { return _inputHandler->GetMouseInputData().GetScrollValue(); }
+        /**
+        * Returns current TriggerState for a mouse button
+        */
         TriggerState GetMouseTriggerState(const int index) const { return _inputHandler-> GetMouseInputData().GetButtonValue().GetTriggerState(index); }
+        /**
+        * Returns current state of all mouse buttons
+        */
         MouseButtonValues GetMouseButtonValues() const { return _inputHandler->GetMouseInputData().GetButtonValue(); }
 
     protected:
