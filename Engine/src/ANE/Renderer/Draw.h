8a3fd52c2d183209b61732a87dbe544529548bd7
Nestorboy <35258953+Nestorboy@users.noreply.github.com>
2024-04-18 20:22:34 +0200

    Added simple drawing structs

---
+++ Engine/src/ANE/Renderer/Draw.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "Mesh.h"
+#include "ANE/Core/Math/Matrix/Matrix4x4.h"
+
+namespace Engine
+{
+    struct DrawCommand
+    {
+        // TODO: Add other necessary draw command information such as material...
+        Matrix4x4 ModelMatrix;
+        uint32_t VertexCount;
+        VmaMeshBuffers MeshBuffers;
+    };
+
+    struct DrawContext
+    {
+        std::vector<DrawCommand> Commands;
+    };
+}
