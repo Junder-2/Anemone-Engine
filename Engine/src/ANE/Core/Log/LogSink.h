f52aa234cf3e691a60683b1fcba4923d3064237b
Junder-2 <pettermikaelsson@gmail.com>
2024-04-18 22:00:52 +0200

    Reworked logging, now stores the 100 latest messages

---
+++ Engine/src/ANE/Core/Log/LogSink.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <spdlog/common.h>
+#include <spdlog/details/log_msg.h>
+
+#include "ANE/Delegate/Delegate.h"
+#include "spdlog/sinks/base_sink.h"
+
+namespace Engine
+{
+    using log_msg = spdlog::details::log_msg;
+
+    class LogSink final : public spdlog::sinks::base_sink<std::mutex>
+    {
+    public:
+        void BindSink(DelegateFree<void(const log_msg&)> delegateMember);
+
+    private:
+        void sink_it_(const log_msg &msg) override;
+        void flush_() override;
+
+        SinglecastDelegate<void(const log_msg&)> _sinkDelegate;
+    };
+}
