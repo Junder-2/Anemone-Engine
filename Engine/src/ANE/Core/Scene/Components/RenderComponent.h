f1f70db8e0dbf78cc97723a2f4c2ee2d84a442e9
Nestorboy <35258953+Nestorboy@users.noreply.github.com>
2024-04-18 20:49:53 +0200

    Prepared RendererComponent for rendering commands

--- Engine/src/ANE/Core/Scene/Components/RenderComponent.h
+++ Engine/src/ANE/Core/Scene/Components/RenderComponent.h
@@ -1,22 +1,28 @@
 ﻿#pragma once
+#include <filesystem>
+
 #include "Component.h"
+#include "ANE/Renderer/Mesh.h"
+#include "ANE/Renderer/Renderer.h"
+
 //todo: finish, this is just a demo
 namespace Engine
 {
     struct RenderComponent : Component
     {
-        // this is a example
-        glm::mat3 Renderer;
-
         //copy constructor
         ANE_COMPONENT_INIT(RenderComponent)
 
-        RenderComponent(const glm::mat3& transform) : Component(typeid(*this).name()) , Renderer(transform) {
-
+    public:
+        RenderComponent(std::string modelPath) : Component(typeid(*this).name()), _modelPath(std::move(modelPath))
+        {
+            Model = Renderer::LoadModel(_modelPath);
         }
 
-        // lets you access the Transform, this is a glm::mat4 with extra fuss
-        operator const glm::mat3&() const { return Renderer; }
-        operator glm::mat3&() { return Renderer; }
+    public:
+        VmaMeshAsset Model = {};
+
+    private:
+        std::string _modelPath;
     };
 }
