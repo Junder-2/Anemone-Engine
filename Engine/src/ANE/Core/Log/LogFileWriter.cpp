e38634461d020e9cc1b45c731933ec433c7d6ae2
Gustav Carlberg <Gustav.a.carlberg@gmail.com>
2024-04-19 20:13:10 +0200

    added logwriter
    added logwriter

---
+++ Engine/src/ANE/Core/Log/LogFileWriter.cpp
@@ -0,0 +1,21 @@
+﻿#include "anepch.h"
+#include "LogFileWriter.h"
+#include "ANE/Core/Entity/UUIDGenerator.h"
+
+
+void Engine::LogFileWriter::InitLogWriter()
+{
+    _sessionUUID = UUIDGenerator::get_uuid();
+}
+
+void Engine::LogFileWriter::WriteToFile(log_msg& msg)
+{
+    if (_sessionUUID != _currentLogUUID)
+    {
+        _currentLogUUID = _sessionUUID;
+
+        // generate new file
+    }
+
+    // append message to log
+}
