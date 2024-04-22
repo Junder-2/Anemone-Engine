dc543672e8c852c6f479dc881dbde0cb2903d6ea
Nestorboy <35258953+Nestorboy@users.noreply.github.com>
2024-04-23 00:47:15 +0200

    Added file for common engine descriptor sets

---
+++ Engine/src/ANE/Renderer/CommonSets.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+#include "ANE/Core/Math/Vector4.h"
+
+namespace Engine
+{
+    struct ApplicationData
+    {
+        Vector4 Time;
+    };
+
+    struct CameraData
+    {
+        Vector4 CameraPosition;
+        Vector4 ScreenParameters;
+    };
+
+    struct SceneData
+    {
+        Vector4 AmbientColor;
+
+        Vector4 SunlightDirection;
+        Vector4 SunlightColor;
+    };
+}
