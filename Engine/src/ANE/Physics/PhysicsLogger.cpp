63d7e4d5c742fe087dc6496b44394681c8c210dd
Junder-2 <pettermikaelsson@gmail.com>
2024-04-22 20:05:25 +0200

    Removing linebreaks from physics logger

--- Engine/src/ANE/Physics/PhysicsLogger.cpp
+++ Engine/src/ANE/Physics/PhysicsLogger.cpp
@@ -5,11 +5,14 @@ namespace Engine
 {
     void PhysicsLogger::log(Level level, const std::string& physicsWorldName, Category category, const std::string& message, const char* filename, int lineNumber)
     {
+        std::string cleanedString = message;
+        std::replace(cleanedString.begin(), cleanedString.end(), '\n', ' ');
+
         switch (level)
         {
-            case Level::Information: ANE_PHYS_LOG_INFO("({0}, {1}): {2}", physicsWorldName, getCategoryName(category), message); break;
-            case Level::Warning: ANE_PHYS_LOG_WARN("({0}, {1}): {2}", physicsWorldName, getCategoryName(category), message); break;
-            case Level::Error: ANE_PHYS_LOG_ERROR("({0}, {1}): {2}", physicsWorldName, getCategoryName(category), message); break;
+            case Level::Information: ANE_PHYS_LOG_INFO("({0}, {1}): {2}", physicsWorldName, getCategoryName(category), cleanedString); break;
+            case Level::Warning: ANE_PHYS_LOG_WARN("({0}, {1}): {2}", physicsWorldName, getCategoryName(category), cleanedString); break;
+            case Level::Error: ANE_PHYS_LOG_ERROR("({0}, {1}): {2}", physicsWorldName, getCategoryName(category), cleanedString); break;
         }
     }
 
