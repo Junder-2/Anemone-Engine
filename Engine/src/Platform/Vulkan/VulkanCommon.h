25f2412b48522d20d4abe20dbdeb5e7cafdb4ec0
Nestorboy <35258953+Nestorboy@users.noreply.github.com>
2024-04-26 14:38:32 +0200

    Added VkResult checking macro

---
+++ Engine/src/Platform/Vulkan/VulkanCommon.h
@@ -0,0 +1,8 @@
+﻿#pragma once
+
+#define CHECK_RESULT(err) ANE_MACRO_SCOPE( \
+{ \
+    if ((err) == VK_SUCCESS) return; \
+    if ((err) < 0) ANE_EASSERT(false, "VkResult is negative ({})", (int)(err)); \
+    else ANE_ELOG_WARN("Vulkan Warning: VkResult is not VK_SUCCESS ({})", (int)(err)); \
+})
