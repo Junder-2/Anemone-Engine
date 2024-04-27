d03ece103706737703614a556d38da899ac37c59
Nestorboy <35258953+Nestorboy@users.noreply.github.com>
2024-04-27 02:33:38 +0200

    Fixed CHECK_RESULT not being usable in non-void functions

--- Engine/src/Platform/Vulkan/VulkanCommon.h
+++ Engine/src/Platform/Vulkan/VulkanCommon.h
@@ -2,7 +2,8 @@
 
 #define CHECK_RESULT(err) ANE_MACRO_SCOPE( \
 { \
-    if ((err) == VK_SUCCESS) return; \
-    if ((err) < 0) ANE_EASSERT(false, "VkResult is negative ({})", (int)(err)); \
-    else ANE_ELOG_WARN("Vulkan Warning: VkResult is not VK_SUCCESS ({})", (int)(err)); \
+    if ((err) != VK_SUCCESS) { \
+        if ((err) < 0) ANE_EASSERT(false, "VkResult is negative ({})", (int)(err)); \
+        else ANE_ELOG_WARN("Vulkan Warning: VkResult is not VK_SUCCESS ({})", (int)(err)); \
+    } \
 })
