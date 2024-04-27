2a79bf5222ff0c4ab34a852469cf98020ddc528b
Nestorboy <35258953+Nestorboy@users.noreply.github.com>
2024-04-27 02:36:44 +0200

    Made CHECK_RESULT only call encapsulated code once

--- Engine/src/Platform/Vulkan/VulkanCommon.h
+++ Engine/src/Platform/Vulkan/VulkanCommon.h
@@ -1,9 +1,10 @@
 ﻿#pragma once
 
-#define CHECK_RESULT(err) ANE_MACRO_SCOPE( \
+#define CHECK_RESULT(x) ANE_MACRO_SCOPE( \
 { \
-    if ((err) != VK_SUCCESS) { \
-        if ((err) < 0) ANE_EASSERT(false, "VkResult is negative ({})", (int)(err)); \
-        else ANE_ELOG_WARN("Vulkan Warning: VkResult is not VK_SUCCESS ({})", (int)(err)); \
+    VkResult err = (x); \
+    if (err != VK_SUCCESS) { \
+        if (err < 0) ANE_EASSERT(false, "VkResult is negative ({})", (int)err); \
+        else ANE_ELOG_WARN("Vulkan Warning: VkResult is not VK_SUCCESS ({})", (int)err); \
     } \
 })
