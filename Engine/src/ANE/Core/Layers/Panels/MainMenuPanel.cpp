b1cce11ac449c3f55a43d70fb0f7142e4cd91057
Junder-2 <pettermikaelsson@gmail.com>
2024-05-02 13:40:59 +0200

    Added ColorUtilities and rework in mainmenu panel

--- Engine/src/ANE/Core/Layers/Panels/MainMenuPanel.cpp
+++ Engine/src/ANE/Core/Layers/Panels/MainMenuPanel.cpp
@@ -10,55 +10,63 @@
 
 namespace Engine
 {
-
-MainMenuPanel::MainMenuPanel(EditorLayer* editorLayer)
-{
-    _editorLayer = editorLayer;
-}
-
-UIUpdateWrapper Engine::MainMenuPanel::OnPanelRender()
-{
-
-    UIUpdateWrapper UIUpdate;
-    if (ImGui::BeginMainMenuBar())
+    MainMenuPanel::MainMenuPanel(EditorLayer* editorLayer)
     {
-        if (ImGui::BeginMenu("File"))
-        {
-            if (ImGui::MenuItem("Create Scene")) UIUpdate.AddPanel(new CreateScenePanel(_editorLayer));
-            if (ImGui::MenuItem("Save Scene"))
-            {
-            } //EnableFlag("CreateSceneWindow");
-            if (ImGui::MenuItem("Load Scene"))
-            {
-            } //EnableFlag("CreateSceneWindow");
-
-
-            ImGui::EndMenu();
-        }
-        if (ImGui::BeginMenu("Debug"))
-        {
-            if (ImGui::MenuItem("Input Analysis"))
-            {
-            } //ToggleFlag("ShowDebugOverlay");
-            if (ImGui::MenuItem("Performance Analysis"))
-            {
-                // Todo: menu
-                // ToggleFlag("ShowDebugOverlay");
-            }
-            ImGui::EndMenu();
-        }
-
-        if (ImGui::BeginMenu("View"))
-        {
-            if (ImGui::MenuItem("Log Window"))  UIUpdate.AddPanel(new EditorLogPanel(_editorLayer));
-            if (ImGui::MenuItem("Hierarchy Window"))  UIUpdate.AddPanel(new SceneHierarchyPanel(_editorLayer));
-            if (ImGui::MenuItem("Inspector Window"))  UIUpdate.AddPanel(new InspectorPanel(_editorLayer));
-
-            ImGui::EndMenu();
-        }
-
-        ImGui::EndMainMenuBar();
+        _editorLayer = editorLayer;
     }
-    return UIUpdate;
-}
+
+    UIUpdateWrapper MainMenuPanel::OnPanelRender()
+    {
+        UIUpdateWrapper UIUpdate;
+        if (ImGui::BeginMainMenuBar())
+        {
+            if (ImGui::BeginMenu("File"))
+            {
+                if (ImGui::MenuItem("Create Scene")) UIUpdate.AddPanel(new CreateScenePanel(_editorLayer));
+                if (ImGui::MenuItem("Save Scene"))
+                {
+                } //EnableFlag("CreateSceneWindow");
+                if (ImGui::MenuItem("Load Scene"))
+                {
+                } //EnableFlag("CreateSceneWindow");
+
+                ImGui::EndMenu();
+            }
+            if (ImGui::BeginMenu("Debug"))
+            {
+                if (ImGui::MenuItem("Input Analysis"))
+                {
+                } //ToggleFlag("ShowDebugOverlay");
+                if (ImGui::MenuItem("Performance Analysis"))
+                {
+                    // Todo: menu
+                    // ToggleFlag("ShowDebugOverlay");
+                }
+                PhysicsAnalysisMenu();
+
+                ImGui::EndMenu();
+            }
+
+            if (ImGui::BeginMenu("View"))
+            {
+                if (ImGui::MenuItem("Log Window"))  UIUpdate.AddPanel(new EditorLogPanel(_editorLayer));
+                if (ImGui::MenuItem("Hierarchy Window"))  UIUpdate.AddPanel(new SceneHierarchyPanel(_editorLayer));
+                if (ImGui::MenuItem("Inspector Window"))  UIUpdate.AddPanel(new InspectorPanel(_editorLayer));
+
+                ImGui::EndMenu();
+            }
+
+            ImGui::EndMainMenuBar();
+        }
+        return UIUpdate;
+    }
+
+    void MainMenuPanel::PhysicsAnalysisMenu()
+    {
+        if (ImGui::BeginMenu("Physics Analysis"))
+        {
+
+            ImGui::EndMenu();
+        }
+    }
 }
