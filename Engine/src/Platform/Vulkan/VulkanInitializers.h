d574d6e17af1d96a58c48be9aedc3ac7daf57d8f
Nestorboy <35258953+Nestorboy@users.noreply.github.com>
2024-05-02 19:07:58 +0200

    Added mip count to ImageViewCreateInfo initializer

--- Engine/src/Platform/Vulkan/VulkanInitializers.h
+++ Engine/src/Platform/Vulkan/VulkanInitializers.h
@@ -28,7 +28,7 @@ namespace VulkanInitializers
     VkPipelineLayoutCreateInfo PipelineLayoutCreateInfo();
 
     VkImageCreateInfo ImageCreateInfo(VkFormat format, VkImageUsageFlags usageFlags, VkExtent3D extent);
-    VkImageViewCreateInfo ImageViewCreateInfo(VkFormat format, VkImage image, VkImageAspectFlags aspectFlags);
+    VkImageViewCreateInfo ImageViewCreateInfo(VkFormat format, VkImage image, VkImageAspectFlags aspectFlags, uint32_t mipLevels = 1);
 
     VkImageSubresourceRange ImageSubresourceRange(VkImageAspectFlags aspectMask);
 }
