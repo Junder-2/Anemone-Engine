07840754123c1719a84be96c0d87ffa1a81b0592
ReneDescarnage <Kyle.dean.sinclair@gmail.com>
2024-05-06 17:20:23 +0200

    added new rendering ui stuff to rendering component for materials

--- Engine/src/ANE/Core/Scene/Components/RenderComponent.h
+++ Engine/src/ANE/Core/Scene/Components/RenderComponent.h
@@ -15,20 +15,44 @@ namespace Engine
 
         RenderComponent(std::string modelPath) : Component(typeid(*this).name()), _modelPath(std::move(modelPath))
         {
+            _materialPropertyBlock.Color = Vector3::OneVector();
+            _materialPropertyBlock.Emission = Vector3::ZeroVector();
+            _materialPropertyBlock.Metallic = 1;
+            _materialPropertyBlock.Normal = 1;
+            _materialPropertyBlock.Height = 1;
+            _materialPropertyBlock.Occlusion = 1;
+            _materialPropertyBlock.Reflectance = 1;
+            _materialPropertyBlock.Roughness = 0;
+
             Model = Renderer::LoadModel(_modelPath);
         }
 
         VmaMeshAsset Model = {};
+        void VerifyDefaultMaterialBlock()
+        {
+            //Could write a "compare to default values" check here and reassign to true
+            _defaultMaterialValues = false;
 
+        }
+
+        void OnValidate()
+        {
+            VerifyDefaultMaterialBlock();
+        }
         static void RegisterComponentMetaData()
         {
             entt::meta<RenderComponent>()
                 .data<&RenderComponent::_modelPath>("Model File Location"_hs).prop("display_name"_hs, "Model File Location")
                 .data<&RenderComponent::Model>("Mesh Asset"_hs).prop("display_name"_hs, "Mesh Asset")
-                .EDITABLE;
+                .EDITABLE
+                .data<&RenderComponent::_materialPropertyBlock>("Material Properties"_hs).prop("display_name"_hs,"Material Properties Block")
+                .EDITABLE
+                .func<&RenderComponent::OnValidate>("OnValidate"_hs);
+;
         }
-
     private:
+        FilamentMetallicRoughness::MaterialConstants _materialPropertyBlock;
+        bool _defaultMaterialValues = true;
         std::string _modelPath;
     };
 }
