52bc63a348324971136939ab705275e9184c3855
Junder-2 <pettermikaelsson@gmail.com>
2024-05-29 22:44:21 +0200

    Added to gizmo class
    -Added comments
    -Additional rotation snapping
    -Setting ImGuizmo style
    -Negative scale working correctly

--- Engine/src/ANE/Utilities/Gizmo.cpp
+++ Engine/src/ANE/Utilities/Gizmo.cpp
@@ -3,8 +3,11 @@
 
 #include <imgui.h>
 #include <ImGuizmo.h>
+
+#include "ColorUtilities.h"
 #include "ANE/Core/Scene/Components/CameraComponent.h"
 #include "ANE/Math/MatrixTypes.h"
+#include "ANE/Math/VMath.h"
 #include "ANE/Renderer/Camera.h"
 
 namespace Engine::Gizmos
@@ -39,6 +42,11 @@ namespace Engine::Gizmos
         _gizmoContext->GlobalSnapValues = values;
     }
 
+    void SetGlobalRotationSnapValues(const Vector3 values)
+    {
+        _gizmoContext->GlobalRotationSnapValues = values;
+    }
+
     void PushUseSnap(const bool enable)
     {
         _gizmoContext->UseSnap = enable;
@@ -47,6 +55,11 @@ namespace Engine::Gizmos
     void PushSnapValue(const Vector3 values)
     {
         _gizmoContext->SnapValues = values;
+    }
+
+    void PushRotationSnapValue(const Vector3 values)
+    {
+        _gizmoContext->RotationSnapValues = values;
     }
 
     void PushTransformSpace(const ImGuizmo::MODE mode)
@@ -70,28 +83,48 @@ namespace Engine::Gizmos
         Matrix4x4 cameraPerspective = Camera::GetCameraComponent().GetPerspectiveMatrix();
         cameraPerspective[1][1] *= -1.f;
         Matrix4x4 cameraView = Camera::GetCameraTransform().GetWorldToLocal();
-        //cameraView[1][1] *= -1.f;
 
         Matrix4x4 transformMatrix = transform->GetLocalToWorld();
+        Matrix4x4 deltaTransformMatrix;
 
         ImGuizmo::Enable(editable);
 
-        ImGuizmo::Manipulate(&(cameraView[0].X), &(cameraPerspective[0].X),
+        ImGuizmo::Manipulate(
+            &(cameraView[0].X), &(cameraPerspective[0].X),
             _gizmoContext->TransformOperation, _gizmoContext->TransformSpace,
-            &(transformMatrix[0].X), nullptr, _gizmoContext->UseSnap ? &_gizmoContext->SnapValues.X : nullptr);
+            &(transformMatrix[0].X), &(deltaTransformMatrix[0].X),
+            _gizmoContext->UseSnap ? &_gizmoContext->SnapValues.X : nullptr,
+            _gizmoContext->UseSnap ? &_gizmoContext->RotationSnapValues.X : nullptr);
 
-        if (ImGuizmo::IsUsing() && editable)
+        if (ImGuizmo::IsUsing() && editable && deltaTransformMatrix != Matrix4x4::Identity())
         {
-            transform->SetPosition(transformMatrix.GetPosition());
+            transform->SetScale(transformMatrix.GetAbsoluteScale() * Math::Sign(transform->GetScale()));
             transform->SetRotation(transformMatrix.GetQuaternion());
-            transform->SetScale(transformMatrix.GetScale());
+            transform->SetPosition(transformMatrix.GetPosition());
         }
     }
 
     void ResetStyle()
     {
+        auto* style = &ImGuizmo::GetStyle();
+
+        style->TranslationLineThickness   = 3.0f; // 3.0f
+        style->TranslationLineArrowSize   = 7.0f; // 6.0f
+        style->RotationLineThickness      = 2.0f; // 2.0f
+        style->RotationOuterLineThickness = 3.0f; // 3.0f
+        style->ScaleLineThickness         = 3.0f; // 3.0f
+        style->ScaleLineCircleSize        = 10.0f; // 6.0f
+        style->HatchedAxisLineThickness   = 0.0f; // 6.0f
+        style->CenterCircleSize           = 5.0f; // 6.0f
+        style->CenterCircleThickness      = 2.5f; // 3.0f
+
+        style->Colors[ImGuizmo::DIRECTION_X] = Vector4(ColorUtilities::HexToRGB(0xC80030), .95f);
+        style->Colors[ImGuizmo::DIRECTION_Y] = Vector4(ColorUtilities::HexToRGB(0x00C830), .95f);
+        style->Colors[ImGuizmo::DIRECTION_Z] = Vector4(ColorUtilities::HexToRGB(0x0030C8), .95f);
+
         _gizmoContext->GlobalUseSnap = false;
         _gizmoContext->GlobalSnapValues = .1f;
+        _gizmoContext->GlobalRotationSnapValues = 45.f;
         PopStyle();
     }
 
@@ -99,6 +132,7 @@ namespace Engine::Gizmos
     {
         _gizmoContext->UseSnap = _gizmoContext->GlobalUseSnap;
         _gizmoContext->SnapValues = _gizmoContext->GlobalSnapValues;
+        _gizmoContext->RotationSnapValues = _gizmoContext->GlobalRotationSnapValues;
 
         _gizmoContext->TransformOperation = ImGuizmo::UNIVERSAL;
         _gizmoContext->TransformSpace = ImGuizmo::LOCAL;
--- Engine/src/ANE/Utilities/Gizmo.h
+++ Engine/src/ANE/Utilities/Gizmo.h
@@ -9,15 +9,17 @@ namespace Engine
 
 namespace Engine::Gizmos
 {
-    struct GizmoContext
+    struct GizmoContext // TODO replace pushing methods with one similar to ImGui
     {
-        bool GlobalUseSnap = false;
-        Vector3 GlobalSnapValues = .1f;
+        bool GlobalUseSnap; // = false
+        Vector3 GlobalSnapValues; // = .1f
+        Vector3 GlobalRotationSnapValues; // = 45.f
 
-        bool UseSnap = false;
-        Vector3 SnapValues = .1f;
-        ImGuizmo::MODE TransformSpace = ImGuizmo::LOCAL;
-        ImGuizmo::OPERATION TransformOperation = ImGuizmo::UNIVERSAL;
+        bool UseSnap; // = false
+        Vector3 SnapValues; // = .1f
+        Vector3 RotationSnapValues; // = 45.f
+        ImGuizmo::MODE TransformSpace; // = ImGuizmo::LOCAL
+        ImGuizmo::OPERATION TransformOperation; // = ImGuizmo::UNIVERSAL
     };
 
     extern GizmoContext* _gizmoContext;
@@ -26,16 +28,29 @@ namespace Engine::Gizmos
     GizmoContext* GetContext();
     void Cleanup();
 
+    /** The default value use snap will use */
     void SetGlobalUseSnap(bool enable);
+    /** The default values snapping will use */
     void SetGlobalSnapValues(Vector3 values);
+    /** The default values rotation snapping will use */
+    void SetGlobalRotationSnapValues(Vector3 values);
 
+    /** Override next use snap value */
     void PushUseSnap(bool enable);
+    /** Override next snapping values */
     void PushSnapValue(Vector3 values);
+    /** Override next rotation snapping values */
+    void PushRotationSnapValue(Vector3 values);
+    /** Override next transform space value */
     void PushTransformSpace(ImGuizmo::MODE mode);
+    /** Override next transform operation value */
     void PushTransformOperation(ImGuizmo::OPERATION operation);
 
+    /** Draws a transform handle using a TransformMatrix */
     void TransformHandle(TransformMatrix* transform, bool editable = true);
 
+    /** Sets all style values to default */
     void ResetStyle();
+    /** Reverts pushed values back to default */
     void PopStyle();
 }
